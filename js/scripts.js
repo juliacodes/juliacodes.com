57d020acf865b97088a2e087e0b1cb120f8df11d
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-03-01 19:01:18 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function Hamburger(){
+    var ham = document.getElementById("nav");
+    var openClose = document.getElementById("openclose");
+
+    ham.classList.toggle("navbar");
+    openClose.classList.toggle("close");
+
+
+
+
+}
